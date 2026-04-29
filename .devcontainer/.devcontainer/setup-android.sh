d08a56e56a8865371a774bfde846324e6933b9db
spik3y666-ctrl <spik3y666@gmail.com>
2026-04-30 00:09:19 +0100

    Add setup script for Android SDK installation
    Added script to install Android SDK components and accept licenses.

--- .devcontainer/.devcontainer/setup-android.sh
+++ .devcontainer/.devcontainer/setup-android.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+yes | sdkmanager --licenses
+
+sdkmanager "platform-tools" \
+           "platforms;android-34" \
+           "build-tools;34.0.0" \
+           "cmdline-tools;latest"
+
+echo "Android SDK installed."
